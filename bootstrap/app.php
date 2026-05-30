ca0d7bb5e0c430a34e2a4a35c90247c2100845be
bawamenewiluther-eng <bawamenewiluther@gmail.com>
2026-05-30 09:31:43 +0700

    1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,32 +3,17 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
-
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware) {
-    $middleware->statefulApi();
-    $middleware->alias([
-
-    'admin' => \App\Http\Middleware\AdminMiddleware::class
-
-]);
-    
-})
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })
     ->withMiddleware(function (Middleware $middleware) {
-
-    $middleware->statefulApi();
-
-})
-    ->create();
+        $middleware->statefulApi(); // <--- WAJIB TAMBAHKAN INI
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
 
